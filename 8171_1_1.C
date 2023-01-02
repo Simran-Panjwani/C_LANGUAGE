a147c916787b05d2c5ac8deb89c7eebf440dc7a1
Simran-Panjwani <rashmi05032001@gmail.com>
2023-01-02 12:11:01 +0530

    all added

---
+++ 8171_1_1.C
@@ -0,0 +1,12 @@
+//Basic Information.
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+   clrscr();
+   printf("*************************\n");
+   printf("*Name:\tSimran\tPanjwani*\n");
+   printf("*GR ID:\t8171\t\t*\n");
+   printf("*************************");
+   getch();
+}
---
+++ 8171_3.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+   clrscr();
+   printf("  - - -\n");
+   printf("|\t|\n");
+   printf("S\t|\n");
+   printf("I\t|\n");
+   printf("M\t|\n");
+   printf("R\t|\n");
+   printf("A\t|\n");
+   printf("N\t|\n");
+   printf("|\t|\n");
+   printf("  - - -\n");
+   getch();
+}
